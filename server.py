9c6874c99372362aae423d8d3ba33defa9a2b2af
Andreas Touliatos <atouliatos43@gmail.com>
2026-07-02 14:49:10 +0100

    Add files via upload

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from routes import bp
 
-app = Flask(__name__, static_folder='public')
+app = Flask(__name__, static_folder='public', static_url_path='')
 app.register_blueprint(bp)
 
 if __name__ == '__main__':
